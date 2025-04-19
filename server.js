b86cd2a71d3aab05faf9509a54a8e2fc4b067557
H-1000 <abdelhamidelnaggar03@gmail.com>
2025-04-19 10:43:43 +0200

    man i hate my life

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 const express = require('express');
-require('dotenv').config();
 const mongoose = require('mongoose'); 
-const mongoURI='mongodb+srv://adminHamid:Test123@cluster0.nr1om.mongodb.net/event-booking?retryWrites=true&w=majority&appName=Cluster07'
-
-console.log('hello')
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log(' MongoDB connected!'))
-  .catch(err => console.error(' MongoDB connection error:', err));
-
+const cors = require('cors'); 
+const cookieParser = require('cookie-parser');
 
 
 const app = express(); //creating an application
+
+const mongoURI='mongodb+srv://adminHamid:Test123@cluster0.nr1om.mongodb.net/event-booking?retryWrites=true&w=majority&appName=Cluster07'
+
+console.log('hello')
 
 app.get('/', (req, res) => {
 
@@ -24,13 +19,53 @@ app.get('/', (req, res) => {
 
 
 const eventRouter = require('./routes/Events'); //importing the events router
-app.use('/events', eventRouter); 
+
 const bookingRouter = require('./routes/booking'); //importing the booking router
-app.use('/booking', bookingRouter); 
+
 const userRouter = require('./routes/User'); //importing the user router   
-app.use('/User', userRouter);
+
 const authRouter = require('./routes/auth')
+
+
+const authenticationMiddleware =require('./Middleware/authenticationMiddleware')
+
+require('dotenv').config();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true,
+    })
+)
+
 app.use('/auth', authRouter);
+app.use(authenticationMiddleware); //using the authentication middleware
+
+app.use('/events', eventRouter); 
+app.use('/booking', bookingRouter); 
+app.use('/User', userRouter);
+
+
+mongoose
+.connect(mongoURI)
+.then(() => {
+    console.log('MongoDB connected!');
+})
+.catch(e => {
+    console.error('MongoDB connection error:', e);
+}); 
+
+app.use(function (req, res, next) {
+    return res.status(404).send("404");
+  });
+ 
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000'); 
