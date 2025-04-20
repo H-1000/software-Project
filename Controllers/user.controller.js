8aab1dbe226e07efbc23378f862880f7fa587b9c
adhambahaa11 <adham.b.barakat@gmail.com>
2025-04-20 20:25:00 +0200

    forgetPassword

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -177,8 +177,29 @@ const userController = {
       console.error("Error updating user role:", error);
       res.status(500).json({ message: "Server error" });
     }
-  }
+  },
   
+  forgetPassword: async (req, res) => {
+    try {
+      const { email, newPassword } = req.body;
+
+      const user = await userModel.findOne({ email });
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      const hashedPassword = await bcrypt.hash(newPassword, 10);
+      user.password = hashedPassword;
+      await user.save();
+
+      res.status(200).json({ message: "Password updated successfully" });
+    } catch (error) {
+      console.error("Error resetting password:", error);
+      res.status(500).json({ message: "Server error" });
+    }
+  }
+
+
   
 };
 
